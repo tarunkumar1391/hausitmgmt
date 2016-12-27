9271c6c0ad6458c322bee25eabadc1baf06ab874
Tarun <tarunkumar1391@outlook.com>
2016-12-27 17:25:17 +0100

    Added update user- backend

--- server/users/updateuser.php
+++ server/users/updateuser.php
@@ -5,3 +5,59 @@
  * Date: 12/27/2016
  * Time: 4:02 PM
  */
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ikp";
+//define variables
+$sno = $fullName = $userEmail  = $userDesignation =$userRoomno = $userDescription = "";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// prepare and bind
+$stmt = $conn->prepare("UPDATE users SET fullName=?, userEmail=?, userDesignation=?, userRoomno=?, userDescription=? WHERE sno=?");
+$stmt->bind_param("sssisi", $fullName, $userEmail, $userDesignation, $userRoomno, $userDescription,$sno);
+
+function input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+// set parameters and execute
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    $fullName = isset($_POST['userfname']) ? input($_POST['userfname']) : "0";
+    $userEmail = isset($_POST['userEmail']) ? input($_POST['userEmail']) : "0";
+    $userDesignation = isset($_POST['userDesignation']) ? input($_POST['userDesignation']) : "0";
+    $userRoomno = isset($_POST['userRoomno']) ? input($_POST['userRoomno']) : "0";
+    $userDescription = isset($_POST['userDescription']) ? input($_POST['userDescription']) : "0";
+    $sno = isset($_POST['sno']) ? input($_POST['sno']) : "0";
+}
+
+
+if ($stmt->execute()) {
+    echo "Entry no. $sno has been update successfully!! ".'\n' ;
+    echo '<a href="../../www/home.php">click here to return!!</a>';
+//    header("Location: ../www/index.html");
+
+} else {
+    die('execute() failed: ' . htmlspecialchars($stmt->error));
+}
+
+//if( true === $stmt){
+//    die('execute() failed: ' . htmlspecialchars($stmt->error));
+//}
+//
+
+//printf ("New records created successfully", $stmt->error);
+
+
+$stmt->close();
+$conn->close();
+?>
